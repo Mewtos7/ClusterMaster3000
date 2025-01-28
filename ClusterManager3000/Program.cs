5e77bb0deabaf6037458a0375cd3639aa62717c2
Mewtos7 <mewtos77@gmail.com>
2025-01-28 14:16:13 +0100

    feature(hetzner api): Add simple deploy and delete of server

--- ClusterManager3000/Program.cs
+++ ClusterManager3000/Program.cs
@@ -1,34 +1,74 @@
-﻿
-using System.Net.Http.Headers;
-
-var mode = "deploy";
-var apiKey = "9X4q746l3KnPrg33a0UK9TDT7DoIKPykwTA2qn9coO6PPI0IGgrE4IWmFoV67cUT";
+﻿using System;
+using System.Text.Json;
+using static System.Net.Mime.MediaTypeNames;
 
 
+namespace ClusterManager3000 {
+    public class Program
+    {
+        private string apiKey = Environment.GetEnvironmentVariable("APIKEY") ?? throw new ArgumentNullException("APIKEY not found");
 
-HttpClient sharedClient = new()
-{
-    BaseAddress = new Uri("https://api.hetzner.cloud/v1/")
-};
+        static async Task Main(string[] args)
+        {
+            var p = new Program();
+            await p.OrchestrateServers();
+        }
+
+        private async Task OrchestrateServers()
+        {
+            var mode = "delete"; //TODO: For testing
+
+            HttpClient sharedClient = new();
+
+            if (mode == "deploy")
+                await deployServer(sharedClient);
+
+            if (mode == "delete")
+                await deleteServer(sharedClient, id: "");
+        }
+
+        public async Task getServers(HttpClient httpClient)
+        {
+            httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {apiKey}");
+
+            using HttpResponseMessage
+                response = await httpClient.GetAsync("https://api.hetzner.cloud/v1/servers");
+
+            var message = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(message);
+        }
+
+        public async Task deployServer(HttpClient httpClient)
+        {
+            httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {apiKey}");
+
+            using StringContent jsonContent = new(
+                JsonSerializer.Serialize(new
+                {
+                    image = "ubuntu-24.04",
+                    name = "test",
+                    server_type = "cx22"
+                }),
+                System.Text.Encoding.UTF8,
+                "application/json");
 
 
+            using HttpResponseMessage
+                response = await httpClient.PostAsync("https://api.hetzner.cloud/v1/servers", jsonContent);
 
-if (mode == "deploy") await deployServer(sharedClient);
-if (mode == "destroy") destroyServer(sharedClient);
+            var message = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(message);
+        }
 
+        public async Task deleteServer(HttpClient httpClient, string id)
+        {
+            httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {apiKey}");
 
+            using HttpResponseMessage
+                response = await httpClient.DeleteAsync($"https://api.hetzner.cloud/v1/servers/{id}");
 
-async Task deployServer(HttpClient httpClient) {
-    httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {apiKey}");
-    using HttpResponseMessage 
-        response = await httpClient.GetAsync("servers");
-    
-    var message = await response.Content.ReadAsStringAsync();
-    Console.WriteLine(message);
-}
-
-
-void destroyServer(HttpClient httpClient)
-{
-
+            var message = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(message);
+        }
+    }
 }
