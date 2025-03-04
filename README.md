9fc577878d0fee374391c69f83c0737da3f3f424
Mewtos7 <39595219+Mewtos7@users.noreply.github.com>
2025-03-04 15:12:49 +0100

    Add new features and update configuration handling (#8)
    Updated README.md to include new features and a roadmap.
    Enhanced AppConfiguration with new properties and updated
    constructor. Modified HetznerServices to use readonly
    AppConfiguration and added SSH key creation. Updated
    Program.cs to orchestrate server creation. Updated
    appsettings.json with new configuration sections.

--- README.md
+++ README.md
@@ -5,7 +5,22 @@ This project should provide an application in c# to create, manage and scale an 
 It should automatically detect high load on affected lxd cluster members (hosts) and add more servers to the lxd cluster and move virtual machines and containers ( with no or minimal constraints.
 
 ## Features implemented
-- Database
-	- SQLite Databases to store informations about lxd cluster members and hosted machines/containers
 - Platform
-	- Hetzner with API Key
+	- Create/Delete Servers with Hetzner API
+	- Create SSHKeys with Hetzner API
+- Database
+	- SQLite Database (Not functional at this point)
+- App configuration
+	- Configuration file for app settings
+
+## Roadmap
+- Next
+	- Save server informations in SQLite database table
+	- Auto-Generate SSH Keys and save them in a secure place
+	- Cloud-init: Upgrade server at start, install lxd and create lxd cluster
+- Mid-target
+	- Add cluster member to existent lxd cluster
+- Future
+	- Nothing in mind :)
+
+
--- src/Helper/AppConfiguration.cs
+++ src/Helper/AppConfiguration.cs
@@ -11,14 +11,20 @@ namespace ClusterManager3000.Helper
     class AppConfiguration
     {
         public string ApiKey { get; }
-        
+        public string Environment {  get; }
+        public string SshKey { get; }
+
         public AppConfiguration()
         {
+            var settingsFile = "appsettings.json";
             var configuration = new ConfigurationBuilder()
                 .SetBasePath(AppContext.BaseDirectory)
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile(settingsFile, optional: false, reloadOnChange: true)
                 .Build();
-            ApiKey = configuration["Provider:Hetzner:APIKEY"] ?? throw new ArgumentNullException("APIKEY not found");
+
+            ApiKey = configuration["provider:hetzner:apiKey"] ?? throw new ArgumentNullException("Apikey not found");
+            Environment = configuration["generalSettings:environment"] ?? throw new ArgumentNullException("Environment not found");
+            SshKey = configuration["provider:hetzner:sshKey"] ?? throw new ArgumentNullException("SSH Key not found");
         }
     }
 }
